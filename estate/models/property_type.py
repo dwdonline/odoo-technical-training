d91c65103265be0adcc650e7263a19a96f585889
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-09-30 16:09:38 +0200

    added property type model

---
+++ estate/models/property_type.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Estate property type model
+
+from odoo import fields, models
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Real Estate Property Type'
+
+    name = fields.Char(required=True)
