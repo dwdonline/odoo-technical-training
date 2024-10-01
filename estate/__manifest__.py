33e09401530d31baf129e458b34aed448b49ae40
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-10-01 10:16:42 +0200

    updates to manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.0.24",  # Version
+    "version": "18.0.0.25",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
-        "views/estate_tag_views.xml",
+        "views/estate_prperty_tag_views.xml",
         "views/estate_property_offer_views.xml",
         "views/estate_menus.xml",
     ],
