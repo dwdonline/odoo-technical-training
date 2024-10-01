9c4c3713e4eae10215376531b81230f3affefc56
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-10-01 15:53:34 +0200

    added color

--- estate/models/property.py
+++ estate/models/property.py
@@ -36,10 +36,10 @@ class EstateProperty(models.Model):
     postcode = fields.Char()
 
     # Many2one relation with the partner model
-    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type" readonly=True)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags" options="{'color_field': 'color'}")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
 
     # Computed total area
--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Real Estate Property Type'
-    _order = 'name'
+    _order = 'sequence, name'
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
