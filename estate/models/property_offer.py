42f5148424332125b65881b48ff2f705bf35a5b4
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-10-01 13:39:28 +0200

    added accepted and refused icons

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -20,3 +20,12 @@ class EstatePropertyOffer(models.Model):
     validity = fields.Integer(string='Offer Validity (days)', default=7, required=True)
     deadline = fields.Date(required=False)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True)
+
+    # The action buttons action_accept_offer and action_refuse_offer are defined in the estate.property model
+    def action_accept_offer(self):
+        self.status = 'accepted'
+        for offer in self:
+            offer.property_id.selling_price = offer.price
+
+    def action_refuse_offer(self):
+        self.status = 'refused'
