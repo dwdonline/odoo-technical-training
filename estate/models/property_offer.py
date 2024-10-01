b3e68846c961f185fc034d5fbfa9ca0d072a75ef
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-10-01 10:06:58 +0200

    updates to offers

--- estate/models/property_offer.py
+++ estate/models/property_offer.py
@@ -17,4 +17,5 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True, ondelete='cascade')
     validity = fields.Integer(string='Offer Validity (days)', default=7, required=True)
+    deadline = fields.Date(required=True)
     currency_id = fields.Many2one('res.currency', string='Currency', required=True)
