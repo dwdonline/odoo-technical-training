0bb12864399ddabf481bb3d9f1b43dc22ef8b22b
Philip N. Deatherage <pd@Philips-MacBook-Pro-2.local>
2024-10-01 15:56:50 +0200

    fixed comma

--- estate/models/property.py
+++ estate/models/property.py
@@ -39,7 +39,7 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one("estate.property.type", string="Property Type", readonly=True)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
-    tag_ids = fields.Many2many("estate.property.tag", string="Tags" options="{'color_field': 'color'}")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags", options="{'color_field': 'color'}")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
 
     # Computed total area
